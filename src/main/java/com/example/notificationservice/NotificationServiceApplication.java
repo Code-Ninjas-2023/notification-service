d1e0f3d6ada747c36b0fcf6dbefe6b7186c2e5d9
Mahlet Seyoum <102604633+MahletSeyoum@users.noreply.github.com>
2023-02-02 18:45:36 -0800

    notification-service and kafka

--- src/main/java/com/example/notificationservice/NotificationServiceApplication.java
+++ src/main/java/com/example/notificationservice/NotificationServiceApplication.java
@@ -1,13 +1,23 @@
 package com.example.notificationservice;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class NotificationServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NotificationServiceApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(KafkaTemplate<String, String> kafkaTemplate){
+        return args -> {
+            kafkaTemplate.send("eCommerce", "Order Confirmed");
+        };
     }
 
 }
